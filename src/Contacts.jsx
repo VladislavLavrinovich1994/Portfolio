3865903a0520a0b80d5a65a502f017c78913ff79
VladislavLavrinovich1994 <markvimand@gmail.com>
2019-11-03 23:30:30 +0300

    initial commit

---
+++ src/Contacts.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import styles from './Contacts.module.css';
+
+function Contacts() {
+  return (
+    <div className={styles.contacts}>
+    
+    </div>
+  );
+}
+export default Contacts;
