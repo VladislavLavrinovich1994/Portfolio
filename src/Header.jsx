a2e67ef2269d517814272cbba79c3b42df9cebbc
VladislavLavrinovich1994 <markvimand@gmail.com>
2019-11-05 15:28:26 +0300

    add head

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import styles from './Header.module.css';
+import NavMenu from './NavMenu';
 
 function Header() {
   return (
     <div className={styles.header}>
-    
+      <div className={styles.container}>
+        <NavMenu/>
+      </div>
+
     </div>
   );
 }
--- src/Main.jsx
+++ src/Main.jsx
@@ -4,7 +4,18 @@ import styles from './Main.module.css';
 function Main() {
   return (
     <div className={styles.main}>
-    
+      <div className={styles.container}>
+      <div className={styles.greeting}>
+        <span>Привет!</span>
+        <span>Меня зовут Владислав </span>
+        <span>Я front-end разработчик</span>
+      </div>
+
+      <div className={styles.photo}>
+        <img src="" alt=""/>
+      </div>
+
+      </div>
     </div>
   );
 }
